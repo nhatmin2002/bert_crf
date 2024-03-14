c377d9c8bf9356954cf6daf320b8c54949f8c86b
nhatmin2002 <134791579+nhatmin2002@users.noreply.github.com>
2024-03-14 16:34:22 +0700

    Update config.py

--- bert-crf4NER/config.py
+++ bert-crf4NER/config.py
@@ -2,14 +2,14 @@ class Config(object):
 	apr_dir = '../model/'
 	data_dir = '../corpus/'
 	model_name = 'model_4.pt'
-	epoch = 5
-	bert_model = 'bert-base-cased'
+	epoch = 20
+	bert_model = 'vinai/phobert-base'
 	lr = 5e-5
 	eps = 1e-8
 	batch_size = 16
 	mode = 'prediction' # for prediction mode = "prediction"
-	training_data = 'eng.train.txt'
-	val_data = 'eng.testa.txt'
-	test_data = 'eng.testa.txt'
+	training_data = 'train.txt'
+	val_data = 'dev.txt'
+	test_data = test.txt'
 	test_out = 'test_prediction.csv'
 	raw_prediction_output = 'raw_prediction.csv'
